8692c6649daa7e2b7dd9c207e6754218caef78f6
Alex DAloisio <alexdaloisio24@gmail.com>
2020-10-07 13:30:12 -0400

    Updates

---
+++ precache-manifest.d46c2c3df6c86516f9302d26117091e7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c797e5c17d14aa262929cb1f3d677627",
+    "url": "/NotYourGrandmas/index.html"
+  },
+  {
+    "revision": "dd4fabd0185be82b5480",
+    "url": "/NotYourGrandmas/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "7e0f3e76de021affaee6",
+    "url": "/NotYourGrandmas/static/css/main.7293a4d8.chunk.css"
+  },
+  {
+    "revision": "dd4fabd0185be82b5480",
+    "url": "/NotYourGrandmas/static/js/2.161e66e3.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/NotYourGrandmas/static/js/2.161e66e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e0f3e76de021affaee6",
+    "url": "/NotYourGrandmas/static/js/main.de761718.chunk.js"
+  },
+  {
+    "revision": "db8bffbfcf3fa457401d",
+    "url": "/NotYourGrandmas/static/js/runtime-main.7e63ec02.js"
+  },
+  {
+    "revision": "32e182e384c82ec11ae2436f3ef04929",
+    "url": "/NotYourGrandmas/static/media/grandma.32e182e3.png"
+  }
+]);
